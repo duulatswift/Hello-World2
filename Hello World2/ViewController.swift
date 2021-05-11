019c0a8d0f876128c854c12c3f30f513204a4abf
Du <duulatb@gmail.com>
2021-05-11 14:49:09 +0600

    Initial Commit

---
+++ Hello World2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World2
+//
+//  Created by Duulat Bikiev on 5/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
