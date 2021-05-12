3ece676e925ccdf3d0221a6c7009713144c04776
Du <duulatb@gmail.com>
2021-05-12 08:04:04 +0600

    1. Start button action 2. Add outlets

--- Hello World2/ViewController.swift
+++ Hello World2/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloLabel: UILabel!
+    @IBOutlet var showButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloLabel.isHidden = true
+        showButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showPressed() {
+        
+        if helloLabel.isHidden {
+            helloLabel.isHidden = false
+            showButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloLabel.isHidden = true
+            showButton.setTitle("Show Text", for: .normal)
+        }
+        
+        
+    }
+    
 }
 
